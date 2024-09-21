7a9f008585be0df6a5d70c1425cddffb610a77bb
Mayur Jadhav <mayurshrikantjadhav@gmail.com>
2024-09-21 22:55:20 +0530

    feat: autodeleting unauthorised session

--- app.py
+++ app.py
@@ -1,54 +1,53 @@
-from flask import Blueprint, request, jsonify, Flask
+from flask import Flask, request, jsonify
 from src.Telegram import Telegram
 import os
-import threading
-from telethon import TelegramClient
+import requests
+import asyncio
 from dotenv import load_dotenv
 from flask_cors import CORS
-import requests 
-import asyncio
 
 load_dotenv()
 
+
 api_id = os.getenv('TELEGRAM_APP_API_ID')
 api_hash = os.getenv('TELEGRAM_APP_API_HASH')
-bot_key= os.getenv('TELEGRAM_BOT_API_KEY')
 node_backend = os.getenv('BACKEND_URL')
-telegram_service = Telegram( api_id, api_hash)
+telegram_service = Telegram(api_id, api_hash)
+
 
 app = Flask(__name__)
-
 CORS(app)
 
-def run_async(coro):
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    return loop.run_until_complete(coro)
 
-def update_backend_with_phone(userId, phone_number):
+
+async def update_backend_with_phone(userId, phone_number):
     try:
         response = requests.post(f"{node_backend}/api/update_phone", json={"userId": userId, "phone_number": phone_number})
         return response.json()
     except Exception as e:
-        print(f"Error updating backend: {str(e)}")
         return {"success": False, "message": "Error updating backend"}
 
 
+
+
 @app.route('/request_otp', methods=['POST'])
-def request_otp():
+async def request_otp():
     phone_number = request.json.get('phone_number')
     userId = request.json.get('userId')
     if not phone_number:
         return jsonify({"error": "Phone number is required."}), 400
+
     try:
-        result = run_async(telegram_service.request_otp(phone_number))
-        threading.Thread(target=update_backend_with_phone, args=(userId, phone_number)).start()
+        result = await telegram_service.request_otp(phone_number)
+        await update_backend_with_phone(userId, phone_number)
         return jsonify(result), 200
     except Exception as e:
         return jsonify({"success": False, "message": "Internal Server Error", "error": str(e)}), 500
 
+
+
 @app.route('/verify_otp', methods=['POST'])
-def verify_otp():
+async def verify_otp():
     phone_number = request.json.get('phone_number')
     otp = request.json.get('otp')
     phone_code_hash = request.json.get('phone_code_hash')
@@ -56,10 +55,12 @@ def verify_otp():
 
     if not phone_number or not otp or not phone_code_hash:
         return jsonify({"error": "Phone number, OTP, and phone_code_hash are required."}), 400
+
     try:
-        result = run_async(telegram_service.verify_otp(phone_number, otp, phone_code_hash, password))
+        result = await telegram_service.verify_otp(phone_number, otp, phone_code_hash, password)
         return jsonify(result), 200
     except Exception as e:
+        app.logger.error(f"Error verifying OTP: {str(e)}")
         return jsonify({"error": f"Error verifying OTP: {str(e)}", "success": False}), 500
 
 
@@ -69,5 +70,7 @@ def verify_otp():
 def index():
     return jsonify({"success": True, "message": "Hello, World!"}), 200
 
+
+
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(host="0.0.0.0", port=5000)
--- bot.py
+++ bot.py
@@ -1,12 +1,16 @@
 import os
 import asyncio
+import logging
 from telethon import TelegramClient, events, functions, types
 from dotenv import load_dotenv
-import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+import re
+import subprocess
 
 load_dotenv()
+
+logging.basicConfig(level=logging.INFO)
 
 bot_token = os.getenv('TELEGRAM_BOT_API_KEY')
 api_id = os.getenv('TELEGRAM_APP_API_ID')
@@ -16,166 +20,165 @@ session_dir = 'store'
 clients = []
 
 
-import re
-
 async def check_command(event, client):
     sender = await event.get_sender()
     me = await client.get_me()
 
-
     if sender.id != me.id:
         return
 
-
-    
-    if (event.message.text.startswith("/group") and event.message.to_id.user_id != sender.id and event.message.from_id.user_id == sender.id):
+    if event.message.text.startswith("/group") and event.message.to_id.user_id != sender.id:
         try:
-            if event.message:
-                kaithia_message = await event.message.edit(f"<b>@kaithia_bot</b> is creating the <code>/group</code>...", parse_mode='HTML')
+            
+            kaithia_message = await event.message.edit(
+                f"<b>@kaithia_bot</b> is creating the <code>/group</code>...",
+                parse_mode='HTML')
 
-                command_parts = event.message.text.split(" ", 1)
-                group_command = command_parts[1] if len(command_parts) > 1 else ""
+            command_parts = event.message.text.split(" ", 1)
 
-                add_kaithia = "@kaithia" in group_command
-                group_command = group_command.replace("@kaithia", "").strip() 
-                
-                recipient_id = event.message.to_id.user_id
-                recipient = await event.client.get_entity(recipient_id)
-                recipient_first_name = getattr(recipient, 'first_name', None)
+            group_command = command_parts[1] if len(command_parts) > 1 else ""
+            add_kaithia = "@kaithia" in group_command
+            group_command = group_command.replace("@kaithia", "").strip()
 
-                group_name = group_command if group_command else f"{sender.first_name} <> {recipient_first_name}"
+            recipient_id = event.message.to_id.user_id
+            recipient = await event.client.get_entity(recipient_id)
+            recipient_first_name = getattr(recipient, 'first_name', None)
 
-                participants = [sender, recipient]
+            group_name = group_command if group_command else f"{sender.first_name} <> {recipient_first_name}"
+            participants = [sender, recipient]
 
-                if add_kaithia:
-                    participants.append("@kaithia_bot")
+            if add_kaithia:
+                participants.append("@kaithia_bot")
 
-                created_group = await event.client(functions.messages.CreateChatRequest(
-                    title=group_name,
-                    users=participants,
-                ))
-                
-                group_details = created_group.stringify()
-                
-                chat_id_match = re.search(r'peerchat\s*\(\s*chat_id\s*=\s*(\d+)\s*\)', group_details, re.IGNORECASE)
-                
-                if chat_id_match:
-                    chat_id = int(chat_id_match.group(1))
-                    
-                    await event.client(functions.messages.EditChatAboutRequest(
+            created_group = await event.client(
+                functions.messages.CreateChatRequest(title=group_name,
+                                                     users=participants))
+            group_details = created_group.stringify()
+            
+            chat_id_match = re.search(r'peerchat\s*\(\s*chat_id\s*=\s*(\d+)\s*\)', group_details, re.IGNORECASE)
+            
+            if chat_id_match:
+                chat_id = int(chat_id_match.group(1))
+                await event.client(functions.messages.EditChatAboutRequest(
                         peer=types.PeerChat(chat_id),
                         about="Created by @kaithia_bot"
-                    ))
-
-                    result = await client(functions.messages.ExportChatInviteRequest(peer=types.PeerChat(chat_id=chat_id), legacy_revoke_permanent=True))
-                    invite_link = result.link
-                    
-                    
-                    await kaithia_message.edit(
-                            f"<b>@kaithia_bot</b> created group <b>{group_name}</b> successfully! \n\n Join the group using the link below: {invite_link}",
-                            parse_mode='HTML'
-                        )
-
-                else:
-                    await kaithia_message.edit(
-                        f"<b>@Kaithia</b> created group <b>{group_name}</b> successfully!",
-                        parse_mode='HTML'
-                    )
+                        ))
+                
+                result = await client(functions.messages.ExportChatInviteRequest(
+                        peer=types.PeerChat(chat_id=chat_id),
+                        legacy_revoke_permanent=True
+                        )) 
+                invite_link = result.link
+                
+                await kaithia_message.edit(f"<b>@kaithia_bot</b> created group <b>{group_name}</b> successfully! \n\n Join the group using the link below: {invite_link}", parse_mode='HTML')
+           
+            else:
+                await kaithia_message.edit(
+                    f"<b>@Kaithia</b> created group <b>{group_name}</b> successfully!",
+                    parse_mode='HTML')
         except Exception as e:
-            print(f"Error creating group: {str(e)}")
+            logging.error(f"Error creating group: {str(e)}")
             if event.message:
-                await event.message.reply(f"Failed to create group: TL says {str(e)}")
+                await event.message.edit(f"Failed to create group: {str(e)}")
+
+
+# it handles new session file created by Flask app
+async def handle_new_session(session_file):
+    logging.info(f"New session file detected: {session_file}. Waiting for 1 minute...")
+    
+    await asyncio.sleep(60)
+    await load_client_for_session(session_file)
 
 
 
-
+# It checks for new session file and loads the client
 async def load_client_for_session(session_file):
+    global clients
+    
     session_path = os.path.join(session_dir, session_file)
-    print(f"Attempting to load session from: {session_path}")
+    logging.info(f"Attempting to load session from: {session_path}")
 
     client = TelegramClient(session_path, api_id, api_hash)
-    max_retries = 15
-    retries = 0
-    while retries < max_retries:
-        try:
-            await client.connect()
-
-            if not await client.is_user_authorized():
-                print(f"Session for {session_file} is not authorized, login required.")
-                await client.disconnect()
-                
-                retries += 1
-                print(f"Retrying to load session for {session_file} in 10 seconds... (Attempt {retries}/{max_retries})")
-                await asyncio.sleep(20)
-                continue 
-            else:
-                print(f"Session for {session_file} authorized and connected!")
-
-            @client.on(events.NewMessage)
-            async def handle_message(event):
-                await check_command(event, client)
-
-            await client.start()
-            print(f"{session_file} connected successfully!")
-            clients.append(client)
-            return 
-
-        except Exception as e:
-            print(f"Failed to connect {session_file}: {str(e)}")
+    try:
+        await client.connect()
+        
+        if not await client.is_user_authorized():
+            logging.warning(f"Session for {session_file} is not authorized, login required.")
             await client.disconnect()
-            return  
+            os.remove(session_path)  # Delete the unauthorized session file
+            logging.info(f"Deleted unauthorized session file: {session_path}")
+            return
+
+        logging.info(f"Session for {session_file} authorized and connected!")
+
+        @client.on(events.NewMessage)
+        async def handle_message(event):
+            await check_command(event, client)
+
+        await client.start()
+        
+        logging.info(f"{session_file} connected successfully!")
+        clients.append(client)
+        
+        
+    except Exception as e:
+        logging.error(f"Failed to connect {session_file}: {str(e)}")
+        await client.disconnect()
 
 
 
-async def main():
-    await load_existing_sessions()
-    await asyncio.gather(*[client.run_until_disconnected() for client in clients])
-
-
-async def load_existing_sessions():
+# creating new client for each session file in /store folder
+async def load_clients():
     session_files = [f for f in os.listdir(session_dir) if f.endswith('_session.session')]
+    
     for session_file in session_files:
         await load_client_for_session(session_file)
 
 
+
+
 class SessionFileEventHandler(FileSystemEventHandler):
+
+    def __init__(self, loop):
+        self.loop = loop
+
     def on_created(self, event):
-        if event.is_directory:
+        # file change event tracking
+        if event.is_directory or not event.src_path.endswith('_session.session'):
             return
-        if event.src_path.endswith('_session.session'):
-            session_file = os.path.basename(event.src_path)
-            print(f"New session file detected: {session_file}")
-            asyncio.run(load_client_for_session(session_file)) 
+        session_file = os.path.basename(event.src_path)
+        asyncio.run_coroutine_threadsafe(handle_new_session(session_file), self.loop)
 
 
-
-
-def start_observer():
-    event_handler = SessionFileEventHandler()
+def start_observer(loop):
+    event_handler = SessionFileEventHandler(loop)
+    
     observer = Observer()
     observer.schedule(event_handler, path=session_dir, recursive=False)
     observer.start()
     return observer
 
 
-async def shutdown_clients():
-    for client in clients:
-        await client.disconnect()
-    print("All clients disconnected.")
-
-if __name__ == "__main__":
-    observer = start_observer()
-
+async def main():
+    loop = asyncio.get_event_loop()
+    observer = start_observer(loop)
+    await load_clients()
     try:
         while True:
-            try:
-                asyncio.run(main())
-            except Exception as e:
-                print(f"An error occurred: {str(e)}")
-                print("Restarting the bot in 5 seconds...")
-                time.sleep(5)
-    except KeyboardInterrupt:
-        observer.stop()
+            await asyncio.sleep(1)
     finally:
-        asyncio.run(shutdown_clients()) 
-    observer.join()
+        observer.stop()
+        observer.join()
+
+
+
+
+if __name__ == "__main__":
+    
+    while True:
+        try:
+            asyncio.run(main())
+        except Exception as e:
+            logging.error(
+                f"Error occurred: {str(e)}. Restarting the script...")
+            subprocess.run(["python3", "bot.py"])
